064edc0f5977991acebd3ce519fd5275fe7425ec
anthony0816 <149899054+anthony0816@users.noreply.github.com>
2025-09-18 23:24:16 -0400

    ...

--- src/app/visit_user_profile/page.jsx
+++ src/app/visit_user_profile/page.jsx
@@ -7,6 +7,7 @@ import AssetsCard from "@/Components/AssetsCard";
 import { GetAssetsByUserId } from "@/utils/functions";
 import ModalShowPicture from "@/Components/ModalShowPicture";
 import ModalAssetData from "@/Components/ModalAssetData";
+import { useSize } from "@/context/resizeContext";
 
 export default function UserProfile() {
   const storage = useData();
@@ -17,12 +18,14 @@ export default function UserProfile() {
   const { currentTheme } = useTheme();
   const auth = useAuth();
   const [page, setPage] = useState(0);
+  const [modalAssetsDataisOpen, setModalAssetsDataisOpen] = useState(false);
   const freeAcces = true;
   const limit = 10;
 
   const ModalShowPictueRef = useRef();
   const ModalAssetOptionsRef = useRef();
   const loadingRef = useRef();
+  const { isMobile } = useSize();
 
   const loadAssets = useCallback(async () => {
     setIsLoading(true);
@@ -92,6 +95,7 @@ export default function UserProfile() {
   function onClickBar(asset) {
     if (ModalAssetOptionsRef.current) {
       const modal = ModalAssetOptionsRef.current;
+      setModalAssetsDataisOpen(true);
       modal.open(asset);
     }
   }
@@ -132,136 +136,149 @@ export default function UserProfile() {
         }}
       />
 
-      <ModalAssetData ref={ModalAssetOptionsRef} onClose={() => null} />
+      <div className="flex flex-row overflow-auto h-[100%]">
+        <div
+          className={` transition-all duration-300 ${
+            modalAssetsDataisOpen ? "w-full" : "w-0"
+          } `}
+        >
+          <ModalAssetData
+            ref={ModalAssetOptionsRef}
+            onClose={() => setModalAssetsDataisOpen(false)}
+          />
+        </div>
 
-      <div
-        className={`${currentTheme.colors.primary} min-h-screen p-1 h-[100%] overflow-auto`}
-      >
-        <div className="max-w-4xl mx-auto">
-          {/* Header del perfil */}
-          <div
-            className={`${currentTheme.colors.third} rounded-lg shadow-lg p-6 mb-6`}
-          >
-            <div className="flex flex-col md:flex-row items-center">
-              <div className="w-32 h-32 mb-4 md:mb-0 md:mr-6">
-                <img
-                  src={user.photoURL}
-                  alt="Foto de perfil"
-                  className={`w-full h-full object-cover rounded-full border-4 ${currentTheme.colors.border}`}
-                />
+        <div
+          className={`${currentTheme.colors.primary} ${
+            modalAssetsDataisOpen ? (isMobile ? "w-0" : "w-full") : "w-full"
+          } p-1 h-[100%] overflow-auto`}
+        >
+          <div className="max-w-4xl mx-auto">
+            {/* Header del perfil */}
+            <div
+              className={`${currentTheme.colors.third} rounded-lg shadow-lg p-6 mb-6`}
+            >
+              <div className="flex flex-col md:flex-row items-center">
+                <div className="w-32 h-32 mb-4 md:mb-0 md:mr-6">
+                  <img
+                    src={user.photoURL}
+                    alt="Foto de perfil"
+                    className={`w-full h-full object-cover rounded-full border-4 ${currentTheme.colors.border}`}
+                  />
+                </div>
+                <div className="text-center md:text-left">
+                  <h1
+                    className={`${currentTheme.textColor.primary} text-3xl font-bold`}
+                  >
+                    {user.displayName}
+                  </h1>
+                  <p className={`${currentTheme.textColor.muted} mt-2`}>
+                    {user.email}
+                  </p>
+                  <div className="flex flex-wrap justify-center md:justify-start mt-4 gap-2">
+                    <span
+                      className={`${currentTheme.colors.secondary} ${currentTheme.textColor.secondary} px-3 py-1 rounded-full text-sm`}
+                    >
+                      Usuario
+                    </span>
+                    <span
+                      className={`${currentTheme.colors.buttonGoogle} text-white px-3 py-1 rounded-full text-sm`}
+                    >
+                      Google
+                    </span>
+                  </div>
+                </div>
               </div>
-              <div className="text-center md:text-left">
-                <h1
-                  className={`${currentTheme.textColor.primary} text-3xl font-bold`}
+            </div>
+            {/* Información de la cuenta */}
+            <div
+              className={`${currentTheme.colors.third} rounded-lg shadow-lg p-6 mb-6`}
+            >
+              <h2
+                className={`${currentTheme.textColor.primary} text-xl font-semibold mb-4`}
+              >
+                Información de la cuenta
+              </h2>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div
+                  className={`${currentTheme.colors.secondary} p-4 rounded-lg`}
                 >
-                  {user.displayName}
-                </h1>
-                <p className={`${currentTheme.textColor.muted} mt-2`}>
-                  {user.email}
-                </p>
-                <div className="flex flex-wrap justify-center md:justify-start mt-4 gap-2">
-                  <span
-                    className={`${currentTheme.colors.secondary} ${currentTheme.textColor.secondary} px-3 py-1 rounded-full text-sm`}
-                  >
-                    Usuario
-                  </span>
-                  <span
-                    className={`${currentTheme.colors.buttonGoogle} text-white px-3 py-1 rounded-full text-sm`}
-                  >
-                    Google
-                  </span>
+                  <p className={`${currentTheme.textColor.muted} text-sm`}>
+                    Provider
+                  </p>
+                  <p className={`${currentTheme.textColor.secondary}`}>
+                    {user.providerData[0]?.providerId || "Google"}
+                  </p>
+                </div>
+
+                <div
+                  className={`${currentTheme.colors.secondary} p-4 rounded-lg`}
+                >
+                  <p className={`${currentTheme.textColor.muted} text-sm`}>
+                    Created at
+                  </p>
+                  <p className={`${currentTheme.textColor.secondary}`}>
+                    {user.metadata.creationTime}
+                  </p>
+                </div>
+
+                <div
+                  className={`${currentTheme.colors.secondary} p-4 rounded-lg`}
+                >
+                  <p className={`${currentTheme.textColor.muted} text-sm`}>
+                    Last seen
+                  </p>
+                  <p className={`${currentTheme.textColor.secondary}`}>
+                    {user.metadata.lastSignInTime}
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-          {/* Información de la cuenta */}
-          <div
-            className={`${currentTheme.colors.third} rounded-lg shadow-lg p-6 mb-6`}
-          >
+
+          {/* Imagenes */}
+          <div className=" ">
             <h2
-              className={`${currentTheme.textColor.primary} text-xl font-semibold mb-4`}
+              className={`${currentTheme.textColor.primary} text-xl font-semibold mb-4 `}
             >
-              Información de la cuenta
+              Assets
             </h2>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div
-                className={`${currentTheme.colors.secondary} p-4 rounded-lg`}
-              >
-                <p className={`${currentTheme.textColor.muted} text-sm`}>
-                  Provider
-                </p>
-                <p className={`${currentTheme.textColor.secondary}`}>
-                  {user.providerData[0]?.providerId || "Google"}
-                </p>
-              </div>
-
-              <div
-                className={`${currentTheme.colors.secondary} p-4 rounded-lg`}
-              >
-                <p className={`${currentTheme.textColor.muted} text-sm`}>
-                  Created at
-                </p>
-                <p className={`${currentTheme.textColor.secondary}`}>
-                  {user.metadata.creationTime}
-                </p>
-              </div>
-
-              <div
-                className={`${currentTheme.colors.secondary} p-4 rounded-lg`}
-              >
-                <p className={`${currentTheme.textColor.muted} text-sm`}>
-                  Last seen
-                </p>
-                <p className={`${currentTheme.textColor.secondary}`}>
-                  {user.metadata.lastSignInTime}
-                </p>
-              </div>
+            <div className="grid gap-6 p-4  [grid-template-columns:repeat(auto-fit,minmax(250px,1fr))]">
+              {assets.map((asset) => (
+                <AssetsCard
+                  key={asset.id}
+                  asset={asset}
+                  currentUserId={auth.user?.uid}
+                  onClickBar={() => onClickBar(asset)}
+                  onClickPhoto={onClickPhoto}
+                />
+              ))}
             </div>
+            {hasMore ? (
+              <div
+                ref={loadingRef}
+                className="h-16 flex flex-col items-center justify-center gap-2 mb-20 "
+              >
+                {/* Spinner */}
+                <div
+                  className={`w-6 h-6 border-4 ${currentTheme.colors.spinner} border-t-transparent rounded-full animate-spin`}
+                ></div>
+                {/* Texto */}
+                <span
+                  className={`text-sm ${currentTheme.colors.subtleText} animate-pulse`}
+                >
+                  Cargando más assets...
+                </span>
+              </div>
+            ) : (
+              <div
+                className={`py-6 text-center text-sm mb-20 ${currentTheme.colors.mutedText}`}
+              >
+                No more items to load
+              </div>
+            )}
           </div>
-        </div>
-
-        {/* Imagenes */}
-        <div className=" ">
-          <h2
-            className={`${currentTheme.textColor.primary} text-xl font-semibold mb-4 `}
-          >
-            Assets
-          </h2>
-          <div className="grid gap-6 p-4  [grid-template-columns:repeat(auto-fit,minmax(250px,1fr))]">
-            {assets.map((asset) => (
-              <AssetsCard
-                key={asset.id}
-                asset={asset}
-                currentUserId={auth.user?.uid}
-                onClickBar={() => onClickBar(asset)}
-                onClickPhoto={onClickPhoto}
-              />
-            ))}
-          </div>
-          {hasMore ? (
-            <div
-              ref={loadingRef}
-              className="h-16 flex flex-col items-center justify-center gap-2 mb-20 "
-            >
-              {/* Spinner */}
-              <div
-                className={`w-6 h-6 border-4 ${currentTheme.colors.spinner} border-t-transparent rounded-full animate-spin`}
-              ></div>
-              {/* Texto */}
-              <span
-                className={`text-sm ${currentTheme.colors.subtleText} animate-pulse`}
-              >
-                Cargando más assets...
-              </span>
-            </div>
-          ) : (
-            <div
-              className={`py-6 text-center text-sm mb-20 ${currentTheme.colors.mutedText}`}
-            >
-              No more items to load
-            </div>
-          )}
         </div>
       </div>
     </>
