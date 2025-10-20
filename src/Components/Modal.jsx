b9cef08a8d8b6434367434c26c2d57f55b6191ae
anthony0816 <149899054+anthony0816@users.noreply.github.com>
2025-10-20 10:54:15 -0400

    Modal version anterior a las animaciones de cerrar y abrir pq daba bateo con las ontificaciones al cambiar de usuario

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,5 +1,3 @@
-import { useClickOutside } from "@/utils/hooks";
-import { useRef, useEffect, useState } from "react";
 export default function Modal({
   children,
   isOpen = false,
@@ -7,44 +5,25 @@ export default function Modal({
   showButtomnClose = true,
   modalAbsolute,
 }) {
-  const modalRef = useRef(null);
-  // que solo se ejecute el onClose si es que esta abireto el modal
-  useClickOutside(modalRef, () => isOpen && onClose());
-
-  const [showChildren, setShowChildren] = useState(true);
-
-  useEffect(() => {
-    let id;
-    if (isOpen) setShowChildren(true);
-    if (!isOpen) {
-      id = setTimeout(() => {
-        setShowChildren(false);
-      }, 300);
-    }
-    return () => clearTimeout(id);
-  }, [isOpen]);
-
-  return (
-    <>
-      <div
-        ref={modalRef}
-        onClick={onClose}
-        className={`  ${
-          modalAbsolute ? "absolute" : "fixed"
-        } inset-0 flex items-center justify-center z-50 bg-black/80 transition-all duration-300 ${
-          isOpen ? "opacity-100 visible  " : "opacity-0 invisible"
-        } `}
-      >
-        {showButtomnClose && (
-          <button
-            onClick={onClose}
-            className="fixed top-4 right-4 text-gray-300 hover:text-gray-800 dark:hover:text-gray-200 transition text-4xl leading-none"
-          >
-            ✕
-          </button>
-        )}
-        {showChildren ? children : ""}
-      </div>
-    </>
-  );
+  if (isOpen)
+    return (
+      <>
+        <div
+          onClick={onClose}
+          className={`${
+            modalAbsolute ? "absolute" : "fixed"
+          } inset-0 flex items-center justify-center z-50 bg-black/80 transition-all duration-300 `}
+        >
+          {showButtomnClose && (
+            <button
+              onClick={onClose}
+              className="fixed top-4 right-4 text-gray-300 hover:text-gray-800 dark:hover:text-gray-200 transition text-4xl leading-none"
+            >
+              ✕
+            </button>
+          )}
+          {children}
+        </div>
+      </>
+    );
 }
