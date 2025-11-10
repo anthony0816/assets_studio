293eed2c1124e4fab20ecde6f621dcd56740275c
anthony0816 <149899054+anthony0816@users.noreply.github.com>
2025-11-10 10:11:07 -0500

    Gemini

--- src/app/api/ia/gg/route.js
+++ src/app/api/ia/gg/route.js
@@ -1,16 +1,17 @@
 import { GoogleGenAI } from "@google/genai";
 import { NextResponse } from "next/server";
-export async function GET() {
+export async function POST(request) {
   try {
+    const { propm } = await request.json();
     const ai = new GoogleGenAI({});
 
     async function main() {
       const response = await ai.models.generateContent({
         model: "gemini-2.5-flash",
-        contents: "Explain how AI works in a few words",
+        contents: propm,
       });
       console.log(response.text);
-      return response;
+      return response.text;
     }
 
     const response = await main();
--- src/app/gg/page.jsx
+++ src/app/gg/page.jsx
@@ -1,25 +1,40 @@
 "use client";
+import { _POST_ } from "@/utils/functions";
 import { useEffect, useState } from "react";
 
 export default function Gemini() {
   const [error, setError] = useState(null);
   const [res, setRes] = useState(null);
-  useEffect(() => {
-    async function main() {
-      fetch("api/ia/gg")
-        .then((res) => res.json())
-        .then(({ res, error }) => {
-          console.log({ error });
-          error && setError(error);
-          console.log({ res });
-        });
-    }
+  const [promp, setPromp] = useState("");
 
-    main();
-  }, []);
+  async function main() {
+    _POST_("api/ia/gg", { promp })
+      .then((res) => res.json())
+      .then(({ res, error }) => {
+        if (error) {
+          setError(error);
+          console.log({ error });
+          return;
+        }
+        console.log({ res });
+        setRes(res);
+      });
+  }
 
   return (
     <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          main();
+        }}
+      >
+        <input
+          onChange={(e) => setPromp(e.target.value)}
+          type="text"
+          className="border border-white m-3  text-white "
+        />
+      </form>
       <p>{res}</p>
       <p className="text-blue-300">{error}</p>
     </>
